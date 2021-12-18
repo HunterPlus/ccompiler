1710c13b8b2118c6363457787aa68408bc257def
HunterPlus <zlw08140957@gmail.com>
2021-12-18 13:50:03 +0800

    Create main.c

---
+++ cc2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr,"%s, invalid number of arguments\n", argv[0]);
+        return 1;
+    }
+
+    char *p = argv[1];
+
+    printf("\t.intel_syntax noprefix\n");
+    printf("\t.global main\n");
+    printf("main:\n");
+    printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("\tadd\trax, %ld\n", strtol(p, &p, 10));            
+        } else if (*p == '-') {
+            p++;
+            printf("\tsub\trax, %ld\n", strtol(p, &p, 10));            
+        } else {
+            fprintf(stderr, "unexpected character: '%c'\n", *p);
+            return 1;
+        }
+    }
+    printf("\tret\n");
+    return 0;
+}
