487090f11ff4ee9a9cb384892d62b58b14961bed
HunterPlus <zlw08140957@gmail.com>
2021-12-18 13:23:35 +0800

    Create main.c

---
+++ cc1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr,"%s, invalid number of arguments\n", argv[0]);
+        return 1;
+    }
+    printf("\t.intel_syntax noprefix\n");
+    printf("\t.global main\n");
+    printf("main:\n");
+    printf("\tmov\trax, %d\n", atoi(argv[1]));
+    printf("\tret\n");
+    return 0;
+}
