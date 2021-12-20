467741a746f91bb1cfcb727b5d5fe433894a7487
HunterPlus <zlw08140957@gmail.com>
2021-12-20 22:24:27 +0800

    Create main.c

---
+++ cc23/main.c
@@ -0,0 +1,13 @@
+#include "c.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) 
+        error("%s, invalid number of arguments", argv[0]);
+
+    Token *tok = tokenize(argv[1]);
+    Function *prog = parse(tok);
+    
+    /* traverse the ast to emit assembly. */
+    codegen(prog);
+    return 0;
+}
